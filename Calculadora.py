6b9496727aa9105ba9aba4cbe67dac3a82c8e170
KaueGuimaraes <76141331+KaueGuimaraes@users.noreply.github.com>
2020-12-25 19:31:30 -0500

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -1,30 +1,144 @@
-print('========= CALCULADORA =========\n')
+print('========= CALCULADORA =========')
 
-r = ''
-numero = 0
-numeros = []
-resultado = 0
-
-print('Pressione 0 para: somar')
-print('Pressione 1 para: subtrair')
-print('Pressione 2 para: dividir')
-print('Pressione 3 para: multiplicar')
-print('Pressione 4 para: taiz quadrada')
-
-r = str(input(': '))
-
-if r == '0':
-    numero = int(input('Quantos números?'))
+while True:
+    r = ''
+    numero = 0
     numeros = []
+    atual = '0'
+    resultado = 0
 
-    for c in range(0, numero):
-        numeros.append(int(input(f"{c + 1} número: ")))
+    print('\nPressione \033[31m0\033[m para: \033[34msomar\033[m')
+    print('Pressione \033[31m1\033[m para: \033[34msubtrair\033[m')
+    print('Pressione \033[31m2\033[m para: \033[34mdividir\033[m')
+    print('Pressione \033[31m3\033[m para: \033[34mmultiplicar\033[m')
+    print('Pressione \033[31m4\033[m para: \033[34mraiz quadrada\033[m')
+    print('Pressione \033[31m5\033[m para: \033[34msair\033[m')
 
-    for c in range(0, len(numeros)):
-         if(c == len(numeros) - 1):
-             print(f'{numeros[c]}', end='')
-         else:
-             print(f'{numeros[c]} + ', end='')
-         numero += numeros[c]
+    r = str(input(': '))
 
-    print(f' = {numero}')
+    if r == '0':
+        print()
+        while True:
+            try:
+                numero = int(input('Quantos números?'))
+                break
+            except:
+                print(end='')
+        numeros = []
+
+        for c in range(0, numero):
+            while True:
+                atual = str(input(f'{c + 1} número: '))
+
+                try:
+                    numeros.append(float(atual))
+                    break
+                except:
+                    print(end='')
+        print()
+        for c in range(0, len(numeros)):
+            resultado += numeros[c]
+            if c == len(numeros) - 1:
+                print(f'{numeros[c]} = ', end='')
+            else:
+                print(f'{numeros[c]} + ', end='')
+
+        print(resultado)
+
+    if r == '1':
+        print()
+        while True:
+            try:
+                numero = int(input('Quantos números?'))
+                break
+            except:
+                print(end='')
+        numeros = []
+
+        for c in range(0, numero):
+            while True:
+                atual = str(input(f'{c + 1} número: '))
+
+                try:
+                    numeros.append(float(atual))
+                    break
+                except:
+                    print(end='')
+        print()
+        for c in range(0, len(numeros)):
+                if c == 0:
+                    resultado = numeros[c]
+                else:
+                    resultado -= numeros[c]
+
+                if c == len(numeros) - 1:
+                    print(f'{numeros[c]} = ', end='')
+                else:
+                    print(f'{numeros[c]} - ', end='')
+
+        print(resultado)
+
+    if r == '2':
+        print()
+        numeros = []
+
+        for c in range(0, 2):
+            while True:
+                atual = str(input(f'{c + 1} número: '))
+
+                try:
+                    numeros.append(float(atual))
+                    break
+                except:
+                    print()
+
+        resultado = numeros[0] / numeros[1]
+        print(f'{numeros[0]} / {numeros[1]} = {resultado}')
+
+    if r == '3':
+        print()
+        while True:
+            try:
+                numero = int(input('Quantos números?'))
+                break
+            except:
+                print(end='')
+        numeros = []
+
+        for c in range(0, numero):
+            while True:
+                atual = str(input(f'{c + 1} número: '))
+
+                try:
+                    numeros.append(float(atual))
+                    break
+                except:
+                    print(end='')
+        print()
+        for c in range(0, len(numeros)):
+            if c == 0:
+                resultado = numeros[c]
+            else:
+                resultado *= numeros[c]
+
+            if c == len(numeros) - 1:
+                print(f'{numeros[c]} = ', end='')
+            else:
+                print(f'{numeros[c]} * ', end='')
+
+        print(resultado)
+
+    if r == '4':
+        while True:
+            atual = str(input('Número: '))
+
+            try:
+                numeros.append(float(atual))
+                break
+            except:
+                print(end='')
+
+        print(f'A raiz quadrada de {numeros[0]} é {numeros[0] ** (1/2)}')
+
+    if r == '5':
+        break
